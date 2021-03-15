fb4079bdbf5b79f96e217a1474cff0e27a97f0e2
leon the leon <gosick237@gmail.com>
2021-03-15 16:38:56 +0900

    story pages are added

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
+const tabsContainer = document.getElementsByClassName("jsMainTab");
 const tabContent = document.getElementsByClassName("jsTabContent");
-const tabLinks = document.getElementsByClassName("jsTabLinks");
+const tabs = document.getElementsByClassName("jsTabLinks");
+const storySelectionPage = document.getElementsByClassName("jsStorySelectPage");
+const storyPage = document.getElementsByClassName("jsStoriesPage");
+const stories = document.getElementsByClassName("jsStories");
+
+function openStory(e, storyName) {
+  console.log(storyName);
+  //turn select page off
+  storySelectionPage[0].style.display = "none";
+  storyPage[0].style.display = "block";
+
+  //turn stories off
+  for (i = 0; i < stories.length; i++) {
+    stories[i].style.display = "none";
+  }
+  /*
+  //set all stories non active
+  for (i = 0; i < stories.length; i++) {
+    stories[i].className = stories[i].className.replace(" active", "");
+  }
+  */
+  //turn on target story on
+  document.getElementById(storyName).style.display = "block";
+  //for (i = 0; i < stories.length; i++) {
+  //  console.log(stories[i].style.display);
+  //}
+}
 
 function openTab(e, tabName) {
-  let i;
   console.log(tabName);
+  storySelectionPage[0].style.display = "block";
+  storyPage[0].style.display = "none";
 
   for (i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = "none";
   }
 
-  for (i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].className = tabs[i].className.replace(" active", "");
   }
 
   document.getElementById(tabName).style.display = "block";
-  e.target.className += "active";
+  e.target.className += " active";
+
+  //for (i = 0; i < tabs.length; i++) {
+  //  console.log(tabs[i].className);
+  //}
 }
 
 document.getElementById("defaultOpen").click();
